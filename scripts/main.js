75aa0926c8da853b8c550496b65e2f0f98f729de
Denys <denys.cheporniuk@gmail.com>
2019-12-04 09:46:11 +0200

    fix message about winner

--- scripts/main.js
+++ scripts/main.js
@@ -51,9 +51,9 @@ class Game {
 
   checkWin() {
     if (this.blocksPosition === [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]) {
-      alert("GREATE!")
+      alert("GREAT!")  
       window.location.reload();
-    }
+    };
 
     return false;
   };
